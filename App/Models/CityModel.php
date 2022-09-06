0a902d8b795fd61ed3b3c29573a2b2ee4f6b3e4c
mhdibaqri <mahdi2000bm@gmail.com>
2022-09-06 10:07:19 +0430

    add api add city

--- App/Models/CityModel.php
+++ App/Models/CityModel.php
@@ -17,6 +17,28 @@ class CityModel {
         $stmt = $this->connection->query( /** @lang query */ "SELECT * FROM city");
         return $stmt->fetchAll();
     }
+    public function find($id)
+    {
+        $stmt = $this->connection->query( /** @lang query */ "SELECT * FROM city");
+        return $stmt->fetchAll();
+    }
+    public function create($cityName , $provinceName)
+    {
+        $sql = ("INSERT INTO `city` (province_id,name) VALUES (:cityName,:provinceName)");
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([':cityName' => $cityName, ':provinceName' => $provinceName]);
+        return $stmt->rowCount();
+    }
+    public function update()
+    {
+        $stmt = $this->connection->query( /** @lang query */ "SELECT * FROM city");
+        return $stmt->fetchAll();
+    }
+    public function delete()
+    {
+        $stmt = $this->connection->query( /** @lang query */ "SELECT * FROM city");
+        return $stmt->fetchAll();
+    }
 
 }
 
--- api/v1/cities/city.php
+++ api/v1/cities/city.php
@@ -4,14 +4,18 @@ include "..\..\..\loader.php";
 use App\Models\CityModel;
 use App\Services\Response;
 
+$cityModel = new CityModel();
+$rowData = json_decode(file_get_contents("php://input"));
+
 switch($_SERVER['REQUEST_METHOD']):
 
     case "GET":
-        $cityModel = new CityModel();
         $cities = $cityModel->findAll();
         Response::respond($cities,Response::HTTP_OK);
 
     case "POST":
+        $result = $cityModel->create($rowData->cityname,$rowData->provincname);
+        Response::respond(["Added successfully, the number of cities added: $result"],Response::HTTP_CREATED);
 
 endswitch;
 
